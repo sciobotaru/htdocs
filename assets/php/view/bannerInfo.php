ec9f5ebfc8e079fa040baf05ca7ee24896492b8a
Ciobotaru Stefan <stfn_ciobotaru@yahoo.com>
2018-08-15 22:56:12 +0300

    CLEAN INDEX.PHP

--- assets/php/view/bannerInfo.php
+++ assets/php/view/bannerInfo.php
@@ -3,9 +3,14 @@ function getBannerInfo()
 {
 ?>
 <div class="banner">
-        <span id="banner-close"> <i class="fa fa-close"></i> </span>
-        <h4 class="message">Telefoane la promotie de la principalele magazine din Romania</h4>
-    <div class="message"><i class="fa fa-heart"></i><span> Oferte actualizate zilnic.</span></div>
+    <span id="banner-close"> 
+        <i class="fa fa-close"></i> 
+    </span>
+    <h4 class="message">Telefoane la promotie de la principalele magazine din Romania</h4>
+    <div class="message">
+        <i class="fa fa-heart"></i>
+        <span> Oferte actualizate zilnic.</span>
+    </div>
 </div>
 <?php
 }
