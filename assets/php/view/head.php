3863b03736fce0722d21fe5f4ee870f63776b559
Ciobotaru Stefan <stfn_ciobotaru@yahoo.com>
2018-08-22 23:08:16 +0300

    layout not fully functional - first version

--- assets/php/view/head.php
+++ assets/php/view/head.php
@@ -2,9 +2,11 @@
 function getHead()
 {
 ?>
+<title>ElectroDeals</title>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>ElectroDeals</title>
+
 <!-- BOOTSTRAP -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/journal/bootstrap.min.css">
@@ -29,14 +31,17 @@ function getHead()
 <!-- Start ProfitShare Zone -->
 <script type="text/javascript" src="assets/js/profitshare.js"></script>
 <!-- End ProfitShare Zone -->   
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=News+Cycle:400,700">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
 <link rel="stylesheet" href="assets/css/styles.min.css">
 <link rel="stylesheet" href="assets/css/product.css">
 <link rel="stylesheet" href="assets/css/global.css">
 <link rel="stylesheet" href="assets/css/cookies.css">
 <link rel="stylesheet" href="assets/css/banner_prezentare.css">
+<link rel="stylesheet" href="assets/css/layout.css">
 <?php
 }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'assets/php/includes.php'; //includes for normal site functioning
-
+//got layout from: https://www.w3schools.com/bootstrap/bootstrap_templates.asp
 checkCookies();
 connectToDatabase();
 ?>
@@ -12,24 +12,51 @@ connectToDatabase();
     </head>
 
     <body>
-        <?php getNavBar(); ?>
-        <div class="menu-area">
+    
+    <?php getNavBar(); ?>
+  
+<div class="container-fluid text-center">   
+  <div class="row content">
 
+<!-- ##########################################-->      
+
+    <!-- Filter area begin -->
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <!-- Filter area end -->
+
+    <!-- Product area begin -->
+    <div class="col-sm-8 text-left"> 
+        <div class="row product-list" id="productList">
+            <?php getProducts(); ?>
         </div>
-        <div class="container">
-            <?php getBannerInfo(); ?>
-            <?php getFilterInfo(); ?>
-            
-            <?php $totalPages = getTotalNumberOfPages(); ?>
-            <div class="row product-list" id="productList">
-                <?php getProducts(); ?>
-            </div>
         <?php getPagination(); ?>
-        </div>
+    </div>
+    <!-- Product area end -->
 
-        <?php getFeatureList(); ?>
-        <?php getFooter(); ?>
-        <?php displayCookiesAlert(); ?>
+    <!-- Promotion area begin -->
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+    <!-- Promotion area end -->
+
+<!-- ##########################################-->      
+
+  </div>
+</div>
+
+    <!-- Footer area begin -->
+    <?php getFooter(); ?>
+    <!-- Footer area end -->
+      
     </body>
 </html>
 
