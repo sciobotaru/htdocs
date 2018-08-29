d4bad14849c04706da15a3a7f7befe2eeb1eb42c
Stefan Ciobotaru <stfn_ciobotaru@yahoo.com>
2018-08-29 17:17:33 +0300

    products card formating

--- assets/php/view/product.php
+++ assets/php/view/product.php
@@ -242,41 +242,26 @@ function getProducts4()
 ?>
 <div class="card-deck" style="padding-top:20px; padding-bottom:20px;">
 
-  <div class="card">
-    <img class="card-img-top" src="" alt="Card image cap" style="height:  200px;">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
+  <div class="card" style="min-width:280px; max-width:280px; height:500px; margin-bottom:25px;">
+
   </div>
 
-  <div class="card">
-    <img class="card-img-top" src=".../100px200/" alt="Card image cap" style="height:  200px;">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
+  <div class="card" style="min-width:280px; max-width:280px; height:500px; margin-bottom:25px;">
 
-  <div class="card">
-    <img class="card-img-top" src=".../100px200/" alt="Card image cap" style="height:  200px;">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
+</div>
 
-<div class="card">
-    <img class="card-img-top" src="" alt="Card image cap" style="height:  200px;">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
+  <div class="card" style="min-width:280px;max-width:280px; height:500px; margin-bottom:25px;">
+
+</div>
+
+  <div class="card" style="min-width:280px;max-width:280px; height:500px; margin-bottom:25px;">
+
+</div>
+
+  <div class="card" style="min-width:280px;max-width:280px; height:500px; margin-bottom:25px;">
+
+</div>
+
 
 </div>
 <?php
