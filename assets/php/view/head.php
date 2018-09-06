153cad4a220b079344ecc9fc5e0872f29f0f28d5
Ciobotaru Stefan <stfn_ciobotaru@yahoo.com>
2018-09-06 22:22:38 +0300

    Removed unused CSS files

--- assets/php/view/head.php
+++ assets/php/view/head.php
@@ -2,34 +2,35 @@
 function getHead()
 {
 ?>
+<!--
 <title>ElectroDeals</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+-->
 <!--<script async src="assets/js/global.js"></script>-->
-<script async src="assets/js/cookies.js"></script>
+<!-- <script async src="assets/js/cookies.js"></script> -->
 <!--<script src="assets/js/script.min.js"></script> -->
 
 <!---SOCIAL  START-->
 <!-- Go to www.addthis.com/dashboard to customize your tools --> 
 <!-- <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a89e7382fbdb8f0"></script> -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113149572-1"></script>
-<script src="assets/js/statistics.js"></script>
+<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113149572-1"></script>
+<script src="assets/js/statistics.js"></script> -->
 <!-- EVENT HANDLERS END  -->
 
 <!-- Start 2performant -->
-<script src="https://cdn.2performant.com/l2/link2.js" id="linkTwoPerformant" data-id="l2/0/1/5/7/4/0/8/2/8/1" data-api-host="https://cdn.2performant.com"></script>
+<!-- <script src="https://cdn.2performant.com/l2/link2.js" id="linkTwoPerformant" data-id="l2/0/1/5/7/4/0/8/2/8/1" data-api-host="https://cdn.2performant.com"></script> -->
 <!-- End 2performant -->
 
 <!-- Start ProfitShare Zone -->
-<script type="text/javascript" src="assets/js/profitshare.js"></script>
+<!-- <script type="text/javascript" src="assets/js/profitshare.js"></script> -->
 <!-- End ProfitShare Zone -->   
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=News+Cycle:400,700">
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=News+Cycle:400,700">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 
@@ -40,7 +41,7 @@ function getHead()
 <link rel="stylesheet" href="assets/css/banner_prezentare.css">
 <link rel="stylesheet" href="assets/css/layout.css">
 <link rel="stylesheet" href="assets/css/sideFilter.css">
-<link rel="stylesheet" href="assets/css/sortDropdown.css">
+<link rel="stylesheet" href="assets/css/sortDropdown.css"> -->
 
 <?php
 }
@@ -103,25 +104,21 @@ function getHeadBootstrap412() //b4 + enter
 function getHeadBootstrap413() //b4 + enter
 {
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>ElectroDeals</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <?php getCustomCSS(); ?>
-    <?php getFontAwesome(); ?>
-    </head>
-  </body>
-</html>
+  <title>ElectroDeals</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
+  <?php getCustomCSS(); ?>
+  <?php getFontAwesome(); ?>
 <?php
 }
 ?>
